27ad48b8327773f66354ac098645f9b68f30f8ab
Sk Ariyan Ali <murselim49@gmail.com>
2026-01-22 07:33:04 +0000

    push

--- Class-1/bisection.c
+++ Class-1/bisection.c
@@ -6,22 +6,18 @@ void main()
 {
     int i = 1;
     float x0, x1, x2, fx0, fx1, fx2;
-
     printf("\nEnter the value of x0: ");
     scanf("%f", &x0);
     printf("\nEnter the value of x1: ");
     scanf("%f", &x1);
     printf("\n Intteration\t x0 \t\t x1 \t\tx2 \t   f(x2) ");
-
     do
     {
         x2 = (x0 + x1) / 2;
         fx0 = f(x0);
         fx1 = f(x1);
         fx2 = f(x2);
-
         printf("\n %d \t %10.4f \t %10.4f \t %10.4f \t %10.4f", i, x0, x1, x2, fx2);
-
         if (fx0 * fx2 < 0)
         {
             x1 = x2;
@@ -32,6 +28,5 @@ void main()
         }
         i++;
     } while (fabs(fx2) > ESP);
-
     printf("\n\n Approx val od root is: %10.4f", x2);
 }
